98eb241a63a6f40726e65ea481239bbb04f8a196
Ashutosh Varma <ashutoshvarma11@live.com>
2020-12-22 00:13:19 +0530

    fix __builtins__ error

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import os
 import sys
 from pathlib import Path
+import builtins
 
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext as _build_ext
@@ -125,7 +126,7 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        builtins.__NUMPY_SETUP__ = False
         import numpy
 
         self.include_dirs.append(numpy.get_include())
